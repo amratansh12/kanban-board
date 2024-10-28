318af8ccfc566393ea50e43c0e1137e811e0acce
amratansh1205 <amratansh.shrivastava@fynmobility.com>
2024-10-28 21:20:25 +0530

    Adding navbar and option

--- src/components/Navbar/DropdownMenu.js
+++ src/components/Navbar/DropdownMenu.js
@@ -1,3 +1,32 @@
+import "./DropdownMenu.css";
+
+export const groupingOptions = ["status", "priority", "user"];
+export const orderingOptions = ["priority", "title"];
+
 export const DropdownMenu = ({ isVisible }) => {
-  return <div className="dropdown-menu">Menu</div>;
+  return (
+    <div className="dropdown-menu">
+      <div className="dropdown-option">
+        <span className="font-bold">Grouping</span>
+        <select>
+          {groupingOptions.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className="dropdown-option">
+        <span className="font-bold">Ordering</span>
+        <select>
+          {orderingOptions.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
 };
